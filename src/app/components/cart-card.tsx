fbcf5d6d4d8732441a030d9ab528c68799e3b4aa
Lucas Santos <lucashfreitas1@gmail.com>
2024-07-29 22:36:07 +1200

    comment next image component in case its a client component

--- src/app/components/cart-card.tsx
+++ src/app/components/cart-card.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { CarType } from "../data/cars";
-import Image from "next/image";
 
 const CarCard: React.FC<{ car: CarType }> = ({ car }) => {
   return (
